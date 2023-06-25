b211374985a72b24fb6ba2e0394b6ac41f899671
CuriousPhysicist <andrewhampson6@gmail.com>
2023-06-25 14:26:46 +0400

    creating personal content and trying out colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     './frontend/javascript/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      "colors": {
+        "saltireblue": {
+          50: "#F5FAFF",
+          100: "#EBF5FF",
+          200: "#D1E8FF",
+          300: "#B3D9FF",
+          400: "#94C9FF",
+          500: "#70B8FF",
+          600: "#42A1FF",
+          700: "#0582FF",
+          800: "#005EB9",
+          900: "#00458A",
+          950: "#003061"
+        }
+      }
+    },
   },
   plugins: [],
 }
